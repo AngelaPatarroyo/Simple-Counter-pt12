573ec0d33e1e3c23862c2a6563cb802c6bcf0f54
melbynrojo <angela.patarroyo@hotmail.com>
2022-11-18 03:15:58 +0000

    iniciado

---
+++ src/js/component/contador.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const seconds = (props) => {
+  return (
+    <div>
+    <div>
+      
+      <div id='main-box'>
+        <div>
+      <i class="bi bi-clock-fill"/>{props.seconds}
+      </div>
+    </div>
+    </div>
+      </div>
+  )
+}
+
+export default seconds
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Contador from "./contador.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    //componentDidMount aqui//
+    var interval = setInterval(() => {
+      setSeconds(seconds + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+  const [seconds, setSeconds] = useState(0);
+
+  return (
+    <div className="container">
+      <Contador seconds={seconds} />
+    </div>
+  );
 };
 
 export default Home;
